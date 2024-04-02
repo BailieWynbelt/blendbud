2cd41a929cb9217dbfa79295a1cdff222728d676
erinberridge <erinberridge@email.arizona.edu>
2024-04-02 15:07:39 -0700

    changes to suggest.py

--- suggest.py
+++ suggest.py
@@ -15,18 +15,6 @@ def get_wine_df(db):
     df.set_index("_id", inplace=True)
     return df
 
-""" TODO : Will return the user's preferred wines as a dataframe, or the user's preferred
-    flavors as a list
-    Right now, just using hard-coded preferred wine_ids and flavors to test suggestions
-"""
-def get_user_prefs(db, user_id):
-    collection = db.preferences
-    query = {"user_id": user_id}
-    cursor = collection.find_one(query)
-    #df = pd.DataFrame(list(cursor))
-    #df.set_index("_id", inplace=True)
-    #return df
-
 """ Creates KMeans clustering model based on numeric wine data
 """
 def create_clusters(df_pref, wine_df, wine_numeric):
@@ -41,8 +29,7 @@ def create_clusters(df_pref, wine_df, wine_numeric):
     @param pref_ids: a list of wine ids (str) marked as 'preferred' by the user
     @return suggestions: a list of wine ids (str) of length 5 to recommend to the user
 """
-def suggest_wine_known_pref(db, pref_ids):
-    wine_df = get_wine_df(db)
+def suggest_wine_known_pref(wine_df, pref_ids):
     df_pref = wine_df.loc[pref_ids]
     wine_numeric = wine_df.select_dtypes(include=np.number)
     wine_numeric = wine_numeric.fillna(wine_numeric.mean())
@@ -64,7 +51,7 @@ def suggest_wine_known_pref(db, pref_ids):
 
     suggestions = trim_suggestions(price_suggestions, df_pref, wine_df)
     suggestions = suggestions.index.to_list()
-    print(suggestions)
+    print("known", suggestions)
     return suggestions
 
 """ Takes initial list of suggestions, removing/adding wines to make it fit to 5 recommended
@@ -76,7 +63,7 @@ def trim_suggestions(initial_suggestions, df_pref, wine_df):
         return new_suggestions.head(5)
     else:
         to_add = wine_df.head(5)
-        new_suggestions = pd.concat([suggestions, to_add])
+        new_suggestions = pd.concat([initial_suggestions, to_add])
         return new_suggestions.head(5)
     return new_suggestions
 
@@ -84,8 +71,7 @@ def trim_suggestions(initial_suggestions, df_pref, wine_df):
     @param pref_flavors: a list of flavors marked as preferred by user during intro quiz
     @return suggestions: a list of wine ids (str) of length 5 to recommend to the user
 """
-def suggest_wine_generic(db, pref_flavors):
-    wine_df = get_wine_df(db)
+def suggest_wine_generic(wine_df, pref_flavors):
     flav_suggestions = pd.DataFrame().reindex_like(wine_df)
     for index, row in wine_df.iterrows():
         if any(flavor in row['flavor_profile'] for flavor in pref_flavors):
@@ -95,36 +81,51 @@ def suggest_wine_generic(db, pref_flavors):
     # for now, suggest the five cheapest options
     top_5 = flav_suggestions.head(5)
     suggestions = top_5.index.to_list()
-    print(suggestions)
+    suggestions = list(map(str, suggestions))
+    print("generic", suggestions)
     return suggestions
+
+""" TODO: failsafe method, recommend 5 top rated wines (hardcoded rn)
+"""
+def suggest_failsafe_wines():
+    suggestions = ["1386686","5177761","1649800","8890662","2472"]
+    return suggestions
+
+""" * Call with string indicating (like/flavor) and corresponding list to use
+    [If the like_pref list is empty, use the flavor_pref list. Even if flavor_pref is empty,
+    there will be a failsafe method implemented]
+    @param indicator: a string ("like"/"flavor") indicating which list from the user's pref is being sent
+           pref_list: an array from the user's preferences (see note above)
+    @return suggestions: a list (length 5) of wine_ids suggested for the user
+"""
+def suggest_wines(indicator, pref_list):
+    client = MongoClient('mongodb+srv://servad:Ta527eZ3eqUWeA9s@tastebud.opgas9v.mongodb.net/')
+    db = client['TasteBud']
+    wine_df = get_wine_df(db)
+    if indicator == "like":
+        return suggest_wine_known_pref(wine_df, pref_list)
+    elif indicator == "flavor":
+        if len(pref_list) == 0:
+            return suggest_failsafe_wines()
+        return suggest_wine_generic(wine_df, pref_list)
+    return suggest_failsafe_wines()
 
 """ Testing Method
 """
-def test_suggestions(db):
+def test_suggestions(wine_df):
     test_top_wines = ["1386686","5177761","1649800","8890662"]
     test_top_flavors = ["vanilla", "oak", "honey", "caramel"]
-    suggest_wine_known_pref(db, test_top_wines) # test for 4 known preferred wines
-    suggest_wine_generic(db, test_top_flavors) # test for 4 known preferred flavors (basic)
+    suggest_wine_known_pref(wine_df, test_top_wines) # test for 4 known preferred wines
+    suggest_wine_generic(wine_df, test_top_flavors) # test for 4 known preferred flavors (basic)
 
-""" TODO : this is the method that will ultimately be called, but doesn't work/is unfinished until
-           preferences table is initialized
-    Call with a user's id (that links to the preferences collection) to get back a list of
-    suggested wine_ids.
-    @param user_id: the user_id that connects to the correct preferences document (w/ like_pref
-    and flavor_pref arrays)
-    @return suggestions: a list (length 5) of wine_ids suggested for the user
+""" Just used for testing  -->
 """
-def suggest_wines(user_id):
-    client = MongoClient('mongodb+srv://servad:Ta527eZ3eqUWeA9s@tastebud.opgas9v.mongodb.net/')
-    db = client['TasteBud']
+#def main():
+#    print("hi")
+#    client = MongoClient('mongodb+srv://servad:Ta527eZ3eqUWeA9s@tastebud.opgas9v.mongodb.net/')
+#    db = client['TasteBud']
+#    wine_df = get_wine_df(db)
+#    test_suggestions(wine_df)
 
-""" Just used for testing right now -->
-"""
-def main():
-    print("hi")
-    client = MongoClient('mongodb+srv://servad:Ta527eZ3eqUWeA9s@tastebud.opgas9v.mongodb.net/')
-    db = client['TasteBud']
-    test_suggestions(db)
-
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
